40cc27c67fa31ce102e0f20de5ae7d3b55720003
RyanHuang25 <huang.renwu@qq.com>
2022-07-13 00:41:08 +0800

    push

--- submitProject_v2/main.py
+++ submitProject_v2/main.py
@@ -11,7 +11,7 @@ import threading
 from flask import Flask,render_template,request
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
-from utils.submitSelenium import SubmitUser
+from utils.submitSelenium import SubmitUser,SubmitUserMany
 
 app = Flask(__name__)
 app.secret_key = "shawroot"
@@ -29,6 +29,9 @@ class Newform(FlaskForm):
     nineSubmit = SubmitField('启动')
     tenSubmit = SubmitField('启动')
 
+class TwoForm(FlaskForm):
+    InputSubmit = SubmitField('启动')
+
 def IndexDealOnlyName(data_list):
     infoList = []
     for data in data_list:
@@ -38,6 +41,14 @@ def IndexDealOnlyName(data_list):
         infoList.append(data)
     SubmitUser().startRequest(infoList)
 
+def TwoIndexDealName(data_list):
+    infoList = []
+    for data in data_list:
+        data = data.replace('\r','')
+        if data == "":
+            continue
+        infoList.append(data)
+    SubmitUserMany().startRequest(infoList)
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -46,8 +57,82 @@ def index():
         if form.oneSubmit.data:
             oneList = request.form.get('one').split('\n')
             IndexDealOnlyName(oneList)
+        elif form.twoSubmit.data:
+            twoList = request.form.get('two').split('\n')
+            IndexDealOnlyName(twoList)
+        elif form.threeSubmit.data:
+            threeList = request.form.get('three').split('\n')
+            IndexDealOnlyName(threeList)
+        elif form.fourSubmit.data:
+            fourList = request.form.get('four').split('\n')
+            IndexDealOnlyName(fourList)
+        elif form.fiveSubmit.data:
+            fiveList = request.form.get('five').split('\n')
+            IndexDealOnlyName(fiveList)
+        elif form.sixSubmit.data:
+            sixList = request.form.get('six').split('\n')
+            IndexDealOnlyName(sixList)
+        elif form.sevenSubmit.data:
+            sevenList = request.form.get('seven').split('\n')
+            IndexDealOnlyName(sevenList)
+        elif form.eightSubmit.data:
+            eightList = request.form.get('eight').split('\n')
+            IndexDealOnlyName(eightList)
+        elif form.nineSubmit.data:
+            nineList = request.form.get('nine').split('\n')
+            IndexDealOnlyName(nineList)
+        elif form.tenSubmit.data:
+            tenList = request.form.get('ten').split('\n')
+            IndexDealOnlyName(tenList)
+        elif form.allSubmit.data:
+            postItem = {
+                "one": request.form.get('one').split('\n'),
+                "two": request.form.get('two').split('\n'),
+                "three": request.form.get('three').split('\n'),
+                "four": request.form.get('four').split('\n'),
+                "five": request.form.get('five').split('\n'),
+                "six": request.form.get('six').split('\n'),
+                "seven": request.form.get('seven').split('\n'),
+                "eight": request.form.get('eight').split('\n'),
+                "nine": request.form.get('nine').split('\n'),
+                "ten": request.form.get('ten').split('\n')
+            }
+            print(postItem)
+            for data in postItem.values():
+                if data == ['']:
+                    continue
+                threading.Thread(target=IndexDealOnlyName,args=(data,)).start()
 
     return render_template('index.html',form=form)
 
+@app.route('/two',methods=['GET','POST'])
+def twoIndex():
+    form = TwoForm()
+    if request.method == "POST":
+        if form.InputSubmit.data:
+            inputList = request.form.get('twoText').split('\n')
+            TwoIndexDealName(inputList)
+    return render_template('two.html',form=form)
+
+@app.route('/three',methods=['GET','POST'])
+def threeIndex():
+    return render_template('three.html')
+
+@app.route('/four',methods=['GET','POST'])
+def fourIndex():
+    return render_template('four.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#############测试数据##################
+'''
+测试号 19999999999 https://jinshuju.net/f/QU9mq1
+测试号 19999999999 https://jinshuju.net/f/TZE1NX
+测试号 19999999999 http://wanxinying.com:80/SubmitInfo/Index?token=on72O0HLstS%2bTrjMnWJuRAXKwhuTKJnjWqtLuQW0yE8pPqn9mCI1hEJzSUrgYMW3cWx%2f70zNL19BT5AMHm5SXl9BOg%2f3hgL0TZhWB7OWgAVXQoHtj07pn5Z7Gwx%2bSJuNYfzN2NypJn9PNZN9zLxBI1iEOH6dORa%2bo8rFTXKWp9c%3d
+测试号 19999999999 http://yongyushiyan.mikecrm.com/WC5vJC1
+测试号 19999999999 http://yongyushiyan.mikecrm.com/ql3KsfK
+测试号 19999999999 http://yongyushiyan.mikecrm.com/Jn6aZwM
+测试号 19999999999 http://yongyushiyan.mikecrm.com/3Sa5QkT
+'''
--- submitProject_v2/utils/submitSelenium.py
+++ submitProject_v2/utils/submitSelenium.py
@@ -6,6 +6,7 @@
 @Email: leo.r.huang@microcore.tech
 @Desc: 
 '''
+import time
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait as Wait
@@ -18,7 +19,7 @@ class SubmitUser:
         option = webdriver.ChromeOptions()
         option.add_experimental_option("detach",True)
         try:
-            self.driver = webdriver.Chrome(executable_path=r'/Users/huangrenwu/Project/submitProject/file/chromedriver')
+            self.driver = webdriver.Chrome(executable_path=r'/Users/huangrenwu/Project/submitProject/submitProject_v2/file/chromedriver',options=option)
         except:
             self.driver = webdriver.Chrome(executable_path=r'D:/Project/submitProject/file/chromedriver.exe',options=option)
         self.driver.maximize_window()
@@ -26,6 +27,11 @@ class SubmitUser:
         # self.driver.get('')
 
     def startRequest(self,data_list):
+        '''
+        数据结构：名字 电话 url
+        :param data_list:
+        :return:
+        '''
         for data in data_list:
             infoList = data.split(' ')
             userName = infoList[0]
@@ -40,6 +46,9 @@ class SubmitUser:
                 self.jinshuju(userName,userPhone)
             elif 'yongyushiyan' in url:
                 self.maike(userName,userPhone)
+        handless = self.driver.window_handles
+        for hand in handless:
+            self.driver.switch_to.window(hand)
 
     def chuangxin(self,userName,userPhone):
         self.driver.find_element_by_id("name").send_keys(userName)
@@ -71,67 +80,65 @@ class SubmitUser:
 
     def maike(self,userName,userPhone):
         try:
+            self.driver.find_element_by_xpath('//input[@name="n"]').clear()
+            self.driver.find_element_by_xpath('//input[@name="n"]').send_keys(userName)
+            self.driver.find_element_by_xpath('//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').clear()
+            self.driver.find_element_by_xpath('//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').send_keys(userPhone)
+            try:
+                selectList = self.driver.find_elements_by_xpath('//ul[@role="radiogroup"]/li')
+                s = len(selectList) - 1
+                status = True
+                while s >= 0:
+                    try:
+                        if selectList[s].find_element_by_xpath('./span/span[2]').text == "配额已满":
+                            s -= 1
+                    except:
+                        status = False
+                        break
+                if status:
+                    title = self.driver.find_element_by_xpath('//div[@class="h_headline"]/span/span').text
+                    print(f"麦克 ===>>> {title} ===>>> {userName} ===>>> 配额已满")
+                    return
+            except:
+                pass
+            self.driver.find_elements_by_xpath('//li[@class="fbc_optionsLi"]')[s].click()
+            self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
+        except:
+            pass
+        try:
+            self.driver.find_element_by_xpath('//input[@name="n"]').clear()
+            self.driver.find_element_by_xpath('//input[@name="n"]').send_keys(userName)
+            self.driver.find_element_by_xpath(
+                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').clear()
+            self.driver.find_element_by_xpath(
+                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').send_keys(userPhone)
+            try:
+                selectList = self.driver.find_elements_by_xpath('//ul[@role="radiogroup"]/li')
+                s = len(selectList) - 1
+                status = True
+                while s >= 0:
+                    try:
+                        if selectList[s].find_element_by_xpath('./span/span[2]').text == "配额已满":
+                            s -= 1
+                    except:
+                        status = False
+                        break
+                if status:
+                    title = self.driver.find_element_by_xpath('//div[@class="h_headline"]/span/span').text
+                    print(f"麦克 ===>>> {title} ===>>> {userName} ===>>> 配额已满")
+                    return
+            except:
+                pass
+            self.driver.find_elements_by_xpath('//li[@class="fbc_optionsLi"]')[s].click()
+            self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
+        except:
+            pass
+        try:
             dataList = self.driver.find_elements_by_xpath('//input[@class="fbi_input aria-content"]')
             for data in dataList:
                 data.clear()
             self.driver.find_elements_by_xpath('//input[@class="fbi_input aria-content"]')[0].send_keys(userName)
             self.driver.find_elements_by_xpath('//input[@class="fbi_input aria-content"]')[1].send_keys(userPhone)
-            self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
-        except:
-            pass
-        try:
-            self.driver.find_element_by_xpath('//input[@name="n"]').clear()
-            self.driver.find_element_by_xpath('//input[@name="n"]').send_keys(userName)
-            self.driver.find_element_by_xpath(
-                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').clear()
-            self.driver.find_element_by_xpath(
-                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').send_keys(userPhone)
-            try:
-                selectList = self.driver.find_elements_by_xpath('//ul[@role="radiogroup"]/li')
-                s = len(selectList) - 1
-                status = True
-                while s >= 0:
-                    try:
-                        if selectList[s].find_element_by_xpath('./span/span[2]').text == "配额已满":
-                            s -= 1
-                    except:
-                        status = False
-                        break
-                if status:
-                    title = self.driver.find_element_by_xpath('//div[@class="h_headline"]/span/span').text
-                    print(f"麦克 ===>>> {title} ===>>> {userName} ===>>> 配额已满")
-                    return
-            except:
-                pass
-            self.driver.find_elements_by_xpath('//li[@class="fbc_optionsLi"]')[-1].click()
-            self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
-        except:
-            pass
-        try:
-            self.driver.find_element_by_xpath('//input[@name="n"]').clear()
-            self.driver.find_element_by_xpath('//input[@name="n"]').send_keys(userName)
-            self.driver.find_element_by_xpath(
-                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').clear()
-            self.driver.find_element_by_xpath(
-                '//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').send_keys(userPhone)
-            try:
-                selectList = self.driver.find_elements_by_xpath('//ul[@role="radiogroup"]/li')
-                s = len(selectList) - 1
-                status = True
-                while s >= 0:
-                    try:
-                        if selectList[s].find_element_by_xpath('./span/span[2]').text == "配额已满":
-                            s -= 1
-                    except:
-                        status = False
-                        break
-                if status:
-                    title = self.driver.find_element_by_xpath('//div[@class="h_headline"]/span/span').text
-                    print(f"麦克 ===>>> {title} ===>>> {userName} ===>>> 配额已满")
-                    return
-            except:
-                pass
-            self.driver.find_elements_by_xpath('//li[@class="fbc_optionsLi"]')[-1].click()
             self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
         except:
             pass
@@ -142,3 +149,78 @@ class SubmitUser:
         except:
             self.driver.refresh()
             self.maike(userName, userPhone)
+
+class SubmitUserMany:
+
+    def __init__(self):
+        option = webdriver.ChromeOptions()
+        option.add_experimental_option("detach",True)
+        try:
+            self.driver = webdriver.Chrome(executable_path=r'/Users/huangrenwu/Project/submitProject/submitProject_v2/file/chromedriver',options=option)
+        except:
+            self.driver = webdriver.Chrome(executable_path=r'D:/Project/submitProject/file/chromedriver.exe',options=option)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+
+    def startRequest(self,data_list):
+        '''
+        数据结构： 名字 电话 次数 url
+        :param data_list:
+        :return:
+        '''
+        for data in data_list:
+            infoList = data.split(' ')
+            userName = infoList[0]
+            userPhone = infoList[1]
+            count = int(infoList[2])
+            url = infoList[3]
+
+            # self.driver.execute_script(f"window.open('{url}')")
+            # self.driver.switch_to.window(self.driver.window_handles[-1])
+            if 'wanxinying' in url:
+                print('创鑫不支持该功能')
+            elif 'jinshuju' in url:
+                self.count = 0
+                self.jinshuju(userName,userPhone,count,url)
+            elif 'yongyushiyan' in url:
+                self.count = 0
+                self.maike(userName,userPhone,count,url)
+        handless = self.driver.window_handles
+        for hand in handless:
+            self.driver.switch_to.window(hand)
+
+    def jinshuju(self,userName,userPhone,count,url):
+        while count > self.count:
+            try:
+                self.driver.find_element_by_xpath('//input[@name="field_1"]').send_keys(userName)
+                self.driver.find_element_by_xpath('//input[@name="field_2"]').send_keys(userPhone)
+                self.driver.find_elements_by_xpath("//div[@class='ant-form-item-control-input-content']/span/div/div")[self.count].click()
+                self.driver.find_element_by_xpath('//div[@class="published-form__footer-buttons"]/button').click()
+            except:
+                pass
+            self.count += 1
+            self.driver.execute_script(f"window.open('{url}')")
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+
+    def maike(self,userName,userPhone,count,url):
+        while count > self.count:
+            self.driver.execute_script(f"window.open('{url}')")
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+            try:
+                self.driver.find_element_by_xpath('//input[@name="n"]').clear()
+                self.driver.find_element_by_xpath('//input[@name="n"]').send_keys(userName)
+                self.driver.find_element_by_xpath('//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').clear()
+                self.driver.find_element_by_xpath('//input[@class="fbi_input aria-content" ][@aria-invalid="false"]').send_keys(userPhone)
+                try:
+                    selectList = self.driver.find_elements_by_xpath('//ul[@role="radiogroup"]/li')
+                    if selectList[self.count].find_element_by_xpath('./span/span[2]').text == "配额已满":
+                        print(f"麦克 ===>>> {selectList[self.count].find_element_by_xpath('./span/span/span/span').text} ===>>> {userName} ===>>> 配额已满")
+                except:
+                    pass
+                self.driver.find_elements_by_xpath('//li[@class="fbc_optionsLi"]')[self.count].click()
+                self.driver.find_element_by_xpath('//div[@class="submit"]/a').click()
+            except:
+                pass
+
+            self.count += 1
+
